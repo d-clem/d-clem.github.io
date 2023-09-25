ebfc9101ea0ac0b7c1121afd342d4e72e590b4a0
d-clem <145859535+d-clem@users.noreply.github.com>
2023-09-25 18:59:09 +0200

    ajout d'un mode sombre

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-let oui = 0;
+
+let buttonSwitch = document.getElementById("buttonSwitch");
+let currentTheme = ""
+
+buttonSwitch.addEventListener("change", changeBackgroundLight);
+
+
+
+function changeBackgroundLight() {
+    if (buttonSwitch.checked) {
+        document.body.style.backgroundColor = "rgb(100,100,100)";
+        document.body.style.color = "white";
+        document.getElementById("navigation").style.backgroundColor = "white";
+
+        document.getElementById("footer").style.backgroundColor = "#2b2d30";
+
+        for (const button of document.getElementsByTagName("button")) {
+            button.style.color = "white";
+            button.style.backgroundColor = "";
+        }
+
+    } else {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+
+        document.getElementById("navigation").style.backgroundColor = "gray";
+
+        document.getElementById("footer").style.backgroundColor = "gray";
+
+        for (const button of document.getElementsByClassName("button-navigation")) {
+            button.style.color = "black";
+            button.style.backgroundColor = "white";
+        }
+    }
+}
+
